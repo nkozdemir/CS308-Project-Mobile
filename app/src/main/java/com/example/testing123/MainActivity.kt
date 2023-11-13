f62bca40a641afd476a5f2100645dc6f706c896a
ege2509 <ekurtulus@sabanciuniv.edu>
2023-11-13 21:21:35 +0300

    Login Page Commit

--- app/src/main/java/com/example/testing123/MainActivity.kt
+++ app/src/main/java/com/example/testing123/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.testing123
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+    fun signInClicked(view: View) {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+    fun signUpClicked(view: View) {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
     }
 }
