df36cb1e64ec2b0ac2f6b94c2bff115bc20a5c4b
ege2509 <ekurtulus@sabanciuniv.edu>
2023-11-15 17:37:26 +0300

    Home Page Commit

--- app/src/main/java/com/example/testing123/HomeActivity.kt
+++ app/src/main/java/com/example/testing123/HomeActivity.kt
@@ -1,11 +1,39 @@
 package com.example.testing123
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.app_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.signout){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun addSongsClicked(view: View){
+        val intent = Intent(this,Add_Song_Activity::class.java)
+        startActivity(intent)
     }
 }
