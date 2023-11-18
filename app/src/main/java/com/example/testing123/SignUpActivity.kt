818c5b50136f0eca4f2a7877dcd3a480147bef86
ege2509 <ekurtulus@sabanciuniv.edu>
2023-11-18 17:07:05 +0300

    Username added to Sign Up Page

--- app/src/main/java/com/example/testing123/SignUpActivity.kt
+++ app/src/main/java/com/example/testing123/SignUpActivity.kt
@@ -11,21 +11,24 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var emailText: EditText
     private lateinit var passwordText1: EditText
     private lateinit var passwordText2: EditText
+    private lateinit var userText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
         emailText = findViewById(R.id.emailText)
+        userText = findViewById(R.id.userText)
         passwordText1 = findViewById(R.id.textPassword)
         passwordText2 =findViewById(R.id.textPassword2)
     }
 
     fun signUpClicked(view: View) {
+        val username = userText.text.toString()
         val email = emailText.text.toString()
         val password1 = passwordText1.text.toString()
         val password2 = passwordText2.text.toString()
-        if (email.isEmpty() || password1.isEmpty()|| password2.isEmpty()) {
-            Toast.makeText(this, "Enter username and password", Toast.LENGTH_SHORT).show()
+        if (username.isEmpty() || email.isEmpty() || password1.isEmpty()|| password2.isEmpty()) {
+            Toast.makeText(this, "Enter username, e-mail and password", Toast.LENGTH_SHORT).show()
             return
         }
         if (password1 != password2) {
